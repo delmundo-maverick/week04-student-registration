7475859f2acc50b0435a52a54ff6742901138536
Maverick Denver S. del Mundo <diemvscode@gmail.com>
2026-08-28 23:05:10 +0800

    Refactor Routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,28 @@
+```php
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\StudentController;
 
+
+/*
+|--------------------------------------------------------------------------
+| Student Registration System Routes
+|--------------------------------------------------------------------------
+*/
+
+// Student List / Home
 Route::get('/', [StudentController::class, 'index'])
     ->name('students.index');
 
+// Registration Form
 Route::get('/register', [StudentController::class, 'create'])
     ->name('students.create');
 
+// Process Registration
 Route::post('/register', [StudentController::class, 'store'])
     ->name('students.store');
 
+// Student Profile
 Route::get('/students/{student}', [StudentController::class, 'show'])
     ->name('students.show');
