9324ffd73b4574d337419b906b2c46b5b759be6a
Maverick Denver S. del Mundo <diemvscode@gmail.com>
2026-08-28 22:13:14 +0800

    Registration Validation Rules

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'student_id' => 'required|unique:students,student_id',
+
+            'first_name' => 'required|string|max:100',
+
+            'middle_name' => 'nullable|string|max:100',
+
+            'last_name' => 'required|string|max:100',
+
+            'email' => 'required|email|unique:students,email',
+
+            'mobile_number' => 'required|numeric',
+
+            'date_of_birth' => 'required|date',
+
+            'gender' => 'required',
+
+            'program' => 'required',
+
+            'year_level' => 'required',
+
+            'address' => 'required|string',
+
+            'profile_picture' => 'required|image|mimes:jpg,jpeg,png|max:2048',
+        ]);
+    }
 }
